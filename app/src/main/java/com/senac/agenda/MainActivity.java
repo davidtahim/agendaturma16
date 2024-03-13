edca8af2b0cce1d052540316975fe08cef9c2f9c
David Tahim <davidtahim@gmail.com>
2024-03-13 16:21:06 -0300

    coloquei os import e a tela de consulta

--- app/src/main/java/com/senac/agenda/MainActivity.java
+++ app/src/main/java/com/senac/agenda/MainActivity.java
@@ -4,6 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.database.sqlite.SQLiteDatabase;
+
+import android.database.Cursor;
+
+import android.widget.*;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
